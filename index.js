fee7e7b79d5af9d217ef141a210237410c0e3bb3
Tinkermannn <wilmansaragih2@gmail.com>
2025-04-13 13:08:21 +0700

    Change ratelimit again

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 
+app.set('trust proxy', 1);
 
 app.use(cors({
     origin: 'https://os.netlabdte.com', 
@@ -16,7 +17,7 @@ app.use(cors({
 
 app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 menit
-    max: 100, // maksimal 100 request per IP per 15 menit
+    max: 500, // maksimal 100 request per IP per 15 menit
     message: {
         success: false,
         message: "Too many requests, please try again later.",
