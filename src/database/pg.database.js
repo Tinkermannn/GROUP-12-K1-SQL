ed7ffdd4104a98fb5f622673df784b9c9f9a76f8
Tinkermannn <wilmansaragih2@gmail.com>
2025-04-13 13:25:59 +0700

    change pool

--- src/database/pg.database.js
+++ src/database/pg.database.js
@@ -2,14 +2,12 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const pool = new Pool({
+const pool = new Pool ({
     connectionString: process.env.PG_CONNECTION_STRING,
-    max: 50,         
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 5000,
-    ssl: { rejectUnauthorized: false },
-  });
-  
+    ssl: {
+        rejectUnauthorized: false,
+    },
+})
 
 const connect = async () => {
     try {
