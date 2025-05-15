bff182a99e47639d0c60eb85c97db0aab9d7e653
Tinkermannn <wilmansaragih2@gmail.com>
2025-05-15 10:49:10 +0700

    Add extended

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 
 const userRoutes = require('./src/routes/user.route');
